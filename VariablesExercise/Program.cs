59d7afa7a328e449fbd069cf3c768500998ed22f
derrickdrye <dryd95@hotmail.com>
2023-06-17 21:04:53 -0500

    VariablesExercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -3,8 +3,21 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   // Declaring and initialize variables
+            string name = "John";
+            int age = 31;
+            char middeInitial = 'D';
+            bool isOver18 = true;
+            double currentTemp = 85.5;
+            decimal currentPrice = 29.99m;
+
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Midde Initial: {middeInitial}");
+            Console.WriteLine($"Is Over 18: {isOver18}");
+            Console.WriteLine($"Current Temp: {currentTemp}");
+            Console.WriteLine($"Current Price: {currentPrice}");
         }
     }
 }
